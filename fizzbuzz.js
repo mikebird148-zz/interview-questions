f7468e6152f647fb28ea09363f7dcbdf9c02a563
Mike Bird <mikebird148@gmail.com>
2016-12-03 19:39:43 -0700

    moving over to my pc

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,24 @@
 // divisible by 3 "Fizz", divisible by 5 "Buzz", divisible by 5 and 3 "Fizz Buzz"
+// it seems like in order to print to the html I may need to add the items to an array and then print to html
 
-function fizzbuzz() {
-    var i = 0;
-    while (i <= 100) {
-        if (i % 3 == 0) {
-            console.log("Fizz");
-        } else if (i % 5 == 0) {
-            console.log("Buzz");
-        } else if (i % 3 && i %5) {
-            console.log("Fizz Buzz");
-        } else {
-            console.log(i);
+    function fizzy() {
+
+        for (var i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                // document.getElementById("fizzbuzz").innerHTML = "Fizz Buzz";
+                console.log("Fizz Buzz");
+            }
+            if (i % 3 == 0) {
+                // document.getElementById("fizzbuzz").innerHTML = "Fizz";
+                console.log("Fizz");
+            } else if (i % 5 == 0) {
+                // document.getElementById("fizzbuzz").innerHTML = "Buzz";
+                console.log("Buzz");
+            } else {
+                // document.getElementById("fizzbuzz").innerHTML = i;
+                console.log(i);
+            }
         }
-        i++;
     }
-}
+
+    fizzy();
---
+++ reverseword.js
@@ -0,0 +1,2 @@
+// given a certain string, 'reverse' for example, write a program that will write 'reverse' in backwords order.
+// in other words, it will print out 'esrever' instead of 'reverse'
